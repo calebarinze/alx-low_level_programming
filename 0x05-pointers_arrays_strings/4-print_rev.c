71ad7cd1d03e7f517d0ffc952255a82005d01fc6
calebarinze <calebarinze77@gmail.com>
2022-10-25 22:07:45 -0700

    filrs

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,4 +4,14 @@
  * print_rev - prints a string in reverse
  * @s: string to be printed
  */
-void _
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar ('\n');
+}
