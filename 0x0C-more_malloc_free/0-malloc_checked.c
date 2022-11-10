5d102b33f1ec0943ae783a2722dc60e516b09564
calebarinze <calebarinze77@gmail.com>
2022-11-10 05:03:12 -0800

    files

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,14 +7,14 @@
  *   * @b: the size of memory to be allocated (sizeof(type) * amount)
  *    *
  *     * Return: pointer to allocated memory.
- *      */
+ */
 
 void *malloc_checked(unsigned int b)
 {
 	int *ptr;
-	
+
 	ptr = malloc(b);
-	
+
 	if (ptr == NULL)
 		exit(98);
 	return (ptr);
