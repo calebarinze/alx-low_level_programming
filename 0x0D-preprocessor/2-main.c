4727cd83812325b41c4c6db9ec6ad2fb00523e1d
calebarinze <calebarinze77@gmail.com>
2022-11-14 05:09:50 -0800

    files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - function that prints the file it was compiled from.
+ *   *
+ *    * Return: Always (0).
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
