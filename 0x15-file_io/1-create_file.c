d31702f621c326d23694e4b521507f592dd62fd5
calebarinze <calebarinze77@gmail.com>
2022-12-05 16:49:28 -0800

    files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,20 +9,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, l;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-	for (l = 0; text_content[l]; l++)
-		;
+	close(o);
 
-	write(fd, text_content, l);
-
-	close(fd);
 	return (1);
 }
